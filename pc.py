8f00022e2d836fa4745b62bb2d89bfe64ea21fa8
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-14 15:45:09 +0700

    first commit

--- pc.py
+++ pc.py
@@ -1,9 +1,19 @@
 from paho.mqtt import client as mqtt # type: ignore
+import time
+import ssl
+
 
 port= 8883
 broker = "ca99add77b634afe8e68917f0339aec6.s1.eu.hivemq.cloud"
+username = "gtechdn"
+password = "Kontum12@"
 
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.verify_mode = ssl.CERT_NONE
+
+
+topic = "gtechdn/sohoa_pc"
 client=mqtt.Client()
 client.connect(broker, port)
-
-client.publish("testing")
+print("Connected to the HIVEMQ Broker")
