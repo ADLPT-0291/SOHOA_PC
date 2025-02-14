79a7358210245a5caef1de5cebc583a0ecc7d160
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-14 15:18:20 +0700

    first commit

--- pc.py
+++ pc.py
@@ -1,1 +1,7 @@
 from paho.mqtt import client as mqtt # type: ignore
+
+port= 8883
+broker = "ca99add77b634afe8e68917f0339aec6.s1.eu.hivemq.cloud"
+
+client=mqtt.Client()
+client.connect(broker, port)
