6e2c32fcc6a6815079f80ade54e2c493eca2d421
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-14 14:52:15 +0700

    first commit

--- pc.py
+++ pc.py
@@ -1,18 +1,3 @@
-#
-# Copyright 2021 HiveMQ GmbH
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import time
 import paho.mqtt.client as paho
 from paho import mqtt
@@ -42,9 +27,9 @@ client.on_connect = on_connect
 # enable TLS for secure connection
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 # set username and password
-client.username_pw_set("gtechdn", "Kontum12@")
+client.username_pw_set("mqtt", "adminmqtt")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("ca99add77b634afe8e68917f0339aec6.s1.eu.hivemq.cloud", 8883)
+client.connect("mqtt.gtechdn.vn", 1883)
 
 # setting callbacks, use separate functions like above for better visibility
 client.on_subscribe = on_subscribe
