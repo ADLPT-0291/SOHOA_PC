9aeebf8d64fceddd2651dc4ec11a37a3c273085d
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-14 16:25:50 +0700

    first commit

--- pc.py
+++ pc.py
@@ -6,8 +6,8 @@ port = 8883  # Cổng TLS
 # port = 1883  # Cổng TLS
 broker = "ca99add77b634afe8e68917f0339aec6.s1.eu.hivemq.cloud"
 broker = "mqtt.gtechdn.vn"
-username = "mqtt"
-password = "adminmqtt"
+username = "gtechdn"
+password = "Kontum12@"
 
 topic = "gtechdn/sohoa_pc"
 
