5edd9a12731e8b2c28e977bd4027add50f928f4e
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-14 15:46:25 +0700

    first commit

--- pc.py
+++ pc.py
@@ -8,12 +8,9 @@ broker = "ca99add77b634afe8e68917f0339aec6.s1.eu.hivemq.cloud"
 username = "gtechdn"
 password = "Kontum12@"
 
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.verify_mode = ssl.CERT_NONE
-
-
 topic = "gtechdn/sohoa_pc"
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# context.verify_mode = ssl.CERT_NONE
 client=mqtt.Client()
 client.connect(broker, port)
 print("Connected to the HIVEMQ Broker")
