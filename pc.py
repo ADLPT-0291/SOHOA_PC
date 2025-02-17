bcf0e967ac8c293fc6b0f3d596c9e2444a627cc0
AD-LPT-0291 <lephuoctruong0220@gmail.com>
2025-02-17 15:59:54 +0700

    first commit

--- pc.py
+++ pc.py
@@ -72,38 +72,37 @@ def api_xacnhanketnoi(data):
   try:
     responsePingtest = requests.post(domainXacnhanketnoi, json = data)
     jsonResponse = responsePingtest.json()
-    if jsonResponse.get('success'):
-        print("✅ Kết nối thành công!")
+    if(jsonResponse['success'] == True):
+      print("✅ Kết nối thành công!")
+      # dieu khien volume #
+      setVolume(jsonResponse['data']['data']['volume'])
+       # Đọc nội dung của tệp cấu hình
+      config.read(CONFIG_FILE)
+      # Thay đổi giá trị input
+      config.set("input", "device", jsonResponse['data']['data']['deviceinput'])
+      config.set("input", "channel", jsonResponse['data']['data']['channel'])
+      config.set("icecast2-0", "bitrate", jsonResponse['data']['data']['bitrate'])
+      config.set("icecast2-0", "server", jsonResponse['data']['data']['serverstream'])
+      config.set("icecast2-0", "port", jsonResponse['data']['data']['portstream'])
+      config.set("icecast2-0", "password", jsonResponse['data']['data']['password'])
+      config.set("icecast2-0", "name", jsonResponse['data']['data']['nameStream'])
+      config.set("icecast2-0", "mountPoint", jsonResponse['data']['data']['mountPoint'])
+      # Ghi lại nội dung vào tệp cấu hình
+      with open(CONFIG_FILE, "w") as configfile:
+        config.write(configfile)
+      # dieu khien play #
+      if(jsonResponse['data']['data']['statusPlay'] == 'play'):   
+        if(jsonResponse['data']['data']['deviceId'] == id):  
+         for proc in subprocess.Popen(['pgrep', '-f', 'darkice'], stdout=subprocess.PIPE).stdout:
+            pid = int(proc.decode())
+            os.kill(pid, signal.SIGTERM)   
+        #  start_darkice() 
+        print("start_darkice")
+      else:
+        # stop_darkice()
+        print("stop_darkice")
     else:
         print("⚠️ Lỗi từ server:", jsonResponse)
-    # if(jsonResponse['success'] == True):
-    #   # dieu khien volume #
-    #   setVolume(jsonResponse['data']['data']['volume'])
-    #    # Đọc nội dung của tệp cấu hình
-    #   config.read(CONFIG_FILE)
-    #   # Thay đổi giá trị input
-    #   config.set("input", "device", jsonResponse['data']['data']['deviceinput'])
-    #   config.set("input", "channel", jsonResponse['data']['data']['channel'])
-    #   config.set("icecast2-0", "bitrate", jsonResponse['data']['data']['bitrate'])
-    #   config.set("icecast2-0", "server", jsonResponse['data']['data']['serverstream'])
-    #   config.set("icecast2-0", "port", jsonResponse['data']['data']['portstream'])
-    #   config.set("icecast2-0", "password", jsonResponse['data']['data']['password'])
-    #   config.set("icecast2-0", "name", jsonResponse['data']['data']['nameStream'])
-    #   config.set("icecast2-0", "mountPoint", jsonResponse['data']['data']['mountPoint'])
-    #   # Ghi lại nội dung vào tệp cấu hình
-    #   with open(CONFIG_FILE, "w") as configfile:
-    #     config.write(configfile)
-    #   # dieu khien play #
-    #   if(jsonResponse['data']['data']['statusPlay'] == 'play'):   
-    #     if(jsonResponse['data']['data']['deviceId'] == id):  
-    #      for proc in subprocess.Popen(['pgrep', '-f', 'darkice'], stdout=subprocess.PIPE).stdout:
-    #         pid = int(proc.decode())
-    #         os.kill(pid, signal.SIGTERM)   
-    #     #  start_darkice() 
-    #     print("start_darkice")
-    #   else:
-    #     # stop_darkice()
-    #     print("stop_darkice")
 #   except:
 #     print('loi xac nhan ket noi')
   except requests.exceptions.RequestException as e:
